f84ebbc2b260a5ef4df53a49eaa0e8f9f78bca1d
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-11-23 14:35:59 +0500

    Task5

---
+++ Tyuiu.AramaAG.Sprint5.Task5.V4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AramaAG.Sprint5.Task5.V4.Lib;
+using System.IO;
+
+namespace Tyuiu.AramaAG.Sprint5.Task5.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Арама А.Г. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Арама Анастасия Георгиевна | СМАРТб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask5V4.txt Найти произведение   *");
+            Console.WriteLine("* всех вещественных чисел в файле. Округлить до трёх знаков после запятой *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask5V4.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.LoadFromDataFile(path));
+            Console.ReadKey();
+        }
+    }
+}
